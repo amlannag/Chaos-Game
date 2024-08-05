557d4fe98e6ad33e929788b6686d441fe32c4324
amlannag <amlannag12@gmail.com>
2024-08-05 21:59:57 +1000

    Update unit_point.py

--- unit_point.py
+++ unit_point.py
@@ -1,9 +1,21 @@
 #Draw N points for the chaos game 
 
 import numpy as np 
+import matplotlib.pyplot as plt
 
 N = 3
 
 r = np.arange(0,N)
 point = np.exp(2.0*np.pi*1j*r/N)
 print(point)
+
+
+res = 100
+w = np.arange(0,res)
+unit_circle = np.exp(2.0*np.pi*w*1j/res)
+
+plt.plot(np.real(unit_circle),np.imag(unit_circle),'b-')
+plt.plot(np.real(point),np.imag(point),'r.')
+
+plt.show()
+print("End")
